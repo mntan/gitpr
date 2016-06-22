fe7384cc847b1489827d96e71be62c87ad46d32f
Stuart Williams <stuwilliams47@gmail.com>
2016-06-22 21:17:47 +0100

    Set default base to master

--- src/args.js
+++ src/args.js
@@ -1,8 +1,8 @@
 var R = require('ramda')
 var argv = require('yargs')
   .usage('Usage: preq --title [title] --body [body] --base [base]')
-    .demand(['title', 'base'])
-    .defaults({ body: '' })
+    .demand(['title'])
+    .defaults({ body: '', base: 'master' })
     .argv
 
 module.exports = R.pick(['title', 'body', 'base'], argv)
